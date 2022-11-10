a5ab954df8fc1267afc20bd68ae368667af1a677
Carol Bradsen <carolbradsen@gmail.com>
2022-11-10 14:13:35 -0700

    Add the functions assignments.

--- cleanUp.js
+++ cleanUp.js
@@ -7,40 +7,50 @@
     // using `node cleanUp.js` or copy and paste into a replit
 
 
-// EX 1:
+// EX 1: a ";" ? at the end of the console.log ()
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
 nameQuestion()
 
 
-// EX 2:
+// EX 2:Tab in line 20 and 21.  
+// Move bottom curly bracket 
+//to the begining of line to be in line with "function."
+
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
 
-// EX 3:
-func makeFreshPesto(){
+// EX 3: Line 32: spell out function, 
+//put space between paragraph() and curly brackets{}
+// line 39/40: move end curly bracket to beginning of line 40
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
 
-//  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+//  EX 4: Line 47: move curly bracket to end of line 46
+// Line 48, 49 fix indents to be same and indented under function.
+// Line 52: moved period . inside of back tick & added a semicolon ; at end
+// Line 53: moved curly bracket } to beginning of line
+// Line 56: added average() to call the function
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum % 2;
 
-  console.log(`the average is: ${avg}`.)
-  }
+  console.log(`the average is: ${avg}.`);
+}
+average(8,9)
--- selfEval.md
+++ selfEval.md
@@ -4,3 +4,8 @@
 - How would you explain to a 5 year old what a function is?
 - Share your understanding of arguments and parameters.
 - What questions/topics (related to Functions) do you want to being to your Small Group?
+
+
+My time management could always be better. I am staying on task (not getting my attention diverted). And working dilligently. The trouble comes with getting tripped up on something small that takes me a while to remember/ figure out. I write down an idea to look into something more, if I figured it out enough to move on, but don't quite understand the problems. I'd love support of any kind. After a lifetime of undiagnosed ADHD, I've been learning time management techniques over the last year, but always am eager to learn more. I appreciate the support of setting up the google calendar and encouragement to think on tiem man. that Turing offers. 
+I will explain functions to my kids after this! A function is a way to define an action that you can use over and over in your code.
+arguments: the actual data, parameters: placeholders
